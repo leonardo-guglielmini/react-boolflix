e235eb8e91bbe1947599d901c78bfd344fcf1203
leonardo-guglielmini <leonardo.guglielmini@outlook.it>
2024-12-14 11:10:52 +0100

    ADD Footer.jsx content

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,29 @@
 import style from "./Footer.module.css"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faDownload } from "@fortawesome/free-solid-svg-icons"
+import { faUser } from "@fortawesome/free-solid-svg-icons"
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons"
+
+import logoSmall from "../../assets/logo-small.png"
 
 export default function Footer(){
     return(
         <footer className={`${style.footer}`}>
-            <p className={style.footerText}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam, cupiditate architecto. Omnis similique incidunt sequi veritatis inventore repellat aliquid nostrum impedit quas fuga id vel, sint provident facilis maiores officia.</p>
+            <div>
+                <img src={logoSmall} alt="logo" />
+                <ul className={style.socials}>
+                    <li><FontAwesomeIcon icon={faDownload} /></li>
+                    <li><FontAwesomeIcon icon={faUser} /></li>
+                    <li><FontAwesomeIcon icon={faEnvelope} /></li>
+                </ul>
+            </div>
+            <p className={style.footerText}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis distinctio velit beatae fugiat a perferendis dicta eveniet. Quae ipsam suscipit blanditiis cupiditate cumque! Reiciendis beatae, et sit nobis sed quibusdam?</p>
+            <ul className={style.info}>
+                <li>Chi siamo</li>
+                <li>Dove andiamo</li>
+                <li>Cosa facciamo</li>
+                <li>Gerry Scotti</li>
+            </ul>
         </footer>
 
     )
