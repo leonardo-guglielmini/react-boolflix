d805a37674684116b04b3d26494bf6c7e66c6f05
leonardo-guglielmini <leonardo.guglielmini@outlook.it>
2024-12-14 10:44:31 +0100

    ADD logo-small, logo-text, scroll arrow list

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,18 +1,20 @@
 import Searchbar from "../Searchbar/Searchbar"
 import style from "./Header.module.css"
 
-import logo from "../../assets/logo.png"
+import logoText from "../../assets/logo-text.png"
+import logoSmall from "../../assets/logo-small.png"
 
 export default function Header(){
     return(
-        <header className={`${style.header} container`}>
-            <a href={""}><img src={logo} className={style.logo}></img></a>
+        <header className={`${style.header}`}>
+            <a href={""}><img src={logoText} className={style.logoText}></img></a>
             <div className={style.pages}>
                 <a href={""}>Latest releases</a>
                 <a href={""}>Upcoming film</a>
                 <a href={""}>Catalogue</a>
             </div>
             <Searchbar/>
+            <a href={""}><img src={logoSmall} className={style.logoSmall}></img></a>
         </header>
     )
 }
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,21 +1,60 @@
-import { useContext } from "react";
+import { useContext, useRef } from "react";
 import GlobalContext from "../../context/GlobalContext";
 import style from "./Main.module.css"
 
 import List from "../List/List";
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faArrowLeft }  from '@fortawesome/free-solid-svg-icons'
+import { faArrowRight }  from '@fortawesome/free-solid-svg-icons'
 
 
 export default function Main(){
 
     const {movies, series} = useContext(GlobalContext)
 
+    const moviesListRef = useRef(null)
+    const seriesListRef = useRef(null)
+
+    const scrollLeft =(target) =>{
+        if(target.current){
+            target.current.scrollBy({
+                top: 0,
+                left: -200,
+                behavior: "smooth"
+            })
+        }
+    }
+
+    const scrollRight =(target) =>{
+        if(target.current){
+            target.current.scrollBy({
+                top: 0,
+                left: 200,
+                behavior: "smooth"
+            })
+        }
+    }
+
     return(
-        <div className={`${style.mainContent} container`}>
+        <div id="list" className={`${style.mainContent} container`}>
             <h1 className={style.title}>MOVIES</h1>
-            <List data={movies}/>
+            <div className={style.section}>
+                <button onClick={() => scrollLeft(moviesListRef)}><FontAwesomeIcon icon={faArrowLeft} className={style.arrow} /></button>
+                <div ref={moviesListRef} className={style.list}>
+                    <List data={movies}/>
+                </div>
+                <button onClick={() => scrollRight(moviesListRef)}><FontAwesomeIcon icon={faArrowRight} className={style.arrow} /></button>
+            </div>
             <hr className={style.separator}/>
             <h1 className={style.title}>TV SERIES</h1>
-            <List data={series}/>
+            <div className={style.section}>
+                <button onClick={() => scrollLeft(seriesListRef)}><FontAwesomeIcon icon={faArrowLeft} className={style.arrow} /></button>
+                <div ref={seriesListRef} className={style.list}>
+                    <List data={series}/>
+                </div>
+                <button onClick={() => scrollRight(seriesListRef)}><FontAwesomeIcon icon={faArrowRight} className={style.arrow} /></button>
+            </div>
         </div>
     )
 }
